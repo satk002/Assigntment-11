e96df96ab264d173395311656b7ac1e6597244e6
Scott Atkins <satki002@odu.edu>
2022-04-23 07:38:06 -0400

    replacement files

---
+++ namelist.h
@@ -0,0 +1,46 @@
+#ifndef NAMELIST_H
+#define NAMELIST_H
+
+#include <iostream>
+#include <string>
+
+
+
+struct NameListNode {
+  std::string name;
+  NameListNode* next;
+
+  NameListNode (std::string newName)
+  {
+    name = newName;
+    next = 0;
+  }
+
+  ~NameListNode() {delete next; next = 0;}
+  void print (std::ostream& out) const;
+};
+
+
+class NameList {
+private:
+  NameListNode* firstNode;
+
+public:
+  NameList () {firstNode = NULL;}
+
+  ~NameList() {clear();}
+
+  void addToFront (std::string newName);
+  void addToEnd (std::string newName);
+
+  void clear();
+
+  void print (std::ostream& out) const;
+};
+
+
+#endif
+
+
+
+
